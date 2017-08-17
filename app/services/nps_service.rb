709610b1645e89a6ef8ece54691a3f70e68bdb9c
AJ <ajoeford@gmail.com>
2017-08-16 20:03:45 -0700

    setup for heroku deploy

--- app/services/nps_service.rb
+++ app/services/nps_service.rb
@@ -1,7 +1,7 @@
 class NpsService
   def importParks
     @resp = Faraday.get 'https://developer.nps.gov/api/v0/parks' do |req|
-      req.headers['Authorization'] = "7CFC0AE5-8AFA-466A-A85D-01E715C13D39"
+      req.headers['Authorization'] = ENV['NPS_KEY']
       req.params['limit'] = 600
     end
     a = JSON.parse(@resp.body)
