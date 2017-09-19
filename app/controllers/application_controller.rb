0bc64d16acb8dd83978cc15a40c68cfdc5abaf39
Andrew Ford <ajoeford@gmail.com>
2017-09-18 20:52:00 -0700

    updated app controller, added sessions controller - for jwt auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,31 @@
 class ApplicationController < ActionController::API
+  # before_action :authenticate
+
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    @current_user ||= User.find(decoded_token["user"]) if auth_present?
+  end
+
+  def authenticate
+    render json: {error: "unauthorized"}, status: 401 unless logged_in?
+  end
+
+
+  private
+
+  def auth_present?
+    !!request.env.fetch("HTTP_AUTHORIZATION","").scan(/Bearer/).flatten.first
+  end
+
+  def decoded_token
+    Auth.decode(token)
+  end
+
+  def token
+    request.env["HTTP_AUTHORIZATION"].scan(/Bearer (.*)$/).flatten.last
+  end
 
 end
