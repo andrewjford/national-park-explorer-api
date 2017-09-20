587934791aea97779903b3b577cd09efde61e8d5
AJ <ajoeford@gmail.com>
2017-09-20 12:27:46 -0700

    commented out authenticate skips

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  skip_before_action :authenticate, only: [:create]
+  # skip_before_action :authenticate, only: [:create]
 
   def create
     user = User.find_by(email: auth_params[:email])
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate, only: [:create]
+  # skip_before_action :authenticate, only: [:create]
 
   def create
     user = User.new(user_params)
